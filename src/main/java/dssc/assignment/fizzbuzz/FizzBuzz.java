80c05031e5f0f272807349f9aab7726416fc26c3
JohnnyBravo10 <115782494+JohnnyBravo10@users.noreply.github.com>
2022-11-01 12:14:08 +0100

    Update FizzBuzz.java

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,22 @@
 package dssc.assignment.fizzbuzz;
-
-public class FizzBuzz {
-
-    public static void main(String... args) {
-        
+public class FizzBuzzBang{
+    public String stampa() {
+        String s="";
+        for (int i=1; i<106; i++){
+            if (i%3==0){
+                s+="Fizz" ;
+            }
+            if (i%5==0){
+                s+="Buzz";
+            }
+            if (i%7==0){
+                s+="Bang";
+            }
+            if (i%3!=0 && i%5!=0 && i%7!=0){
+                s+=Integer.toString(i);
+            }
+            s+=" ";
+        }
+        return s;
     }
-
 }
